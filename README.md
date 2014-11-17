6b569b3e163946cfc38bae5c5878b09277efa455
Manuel Roth <manuelroth@hotmail.ch>
2014-11-17 20:37:57 +0100

    Implemented unit test for all arithmetic expressions

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-bind
-====
+# Bind expressions and standard functors 
+
+Bind expressions for arithmetic expressions (x = first argument, y = second argument) by using standard functors and functions.
+
+## Project Setup for Cevelop
+
+1. Install [Cevelop](http://cevelop.com/)
+2. Open the repository as a workspace
+3. Import as existing projects into workspace
+
+## Build Project
+
+You can use Eclipse to build the project or the provided makefile.
+
+```
+make
+```
+
+## Run Tests
+
+We use [CUTE](http://cute-test.com/) for our unit tests.
+To run the tests you need to compile the tests and then simply run the
+executable. This will output the test results to the console
+and a XML file.
+
+```
+make test
+```
--- bindtest/src/Test.cpp
+++ bindtest/src/Test.cpp
@@ -2,15 +2,68 @@
 #include "ide_listener.h"
 #include "xml_listener.h"
 #include "cute_runner.h"
+#include <functional>
+#include <math.h>
 
-void thisIsATest() {
-	ASSERTM("start writing tests", false);	
+using namespace std::placeholders;
+
+void testSumFunctor() {
+	auto sumFunc = bind(std::plus<double>{},_1,_2);
+	double x{1.0}, y{2.0};
+	double expected{x+y};
+	ASSERT_EQUAL(expected, sumFunc(x,y));
+}
+
+void testTerm(){
+	auto multiplyFunc = bind(std::multiplies<double>{}, 2, _1);
+	auto divideFunc = bind(std::divides<double>{},_2,3);
+	auto combinedFunc = bind(std::minus<double>{},multiplyFunc,divideFunc);
+	double x{2.0}, y{3.0};
+	double expected{(2 * x) - (y / 3)};
+	ASSERT_EQUAL(expected, combinedFunc(x,y));
+}
+
+void testModulo(){
+	auto squareFunc = bind(std::multiplies<int>{},_1,_1);
+	auto modulusFunc = bind(std::modulus<int>{}, squareFunc, _2);
+	int x{2}, y{2};
+	int expected{(x * x) % y};
+	ASSERT_EQUAL(expected, modulusFunc(x,y));
+}
+
+void testSquare(){
+	auto squareFunc = bind(std::multiplies<double>{},_1,_1);
+	double x{2};
+	double expected{x*x};
+	ASSERT_EQUAL(expected,squareFunc(x));
+}
+
+void testQuadraticEquation(){
+	// (x/2)
+	auto divideFunc = bind(std::divides<double>{},_1,2.0);
+	// ((x/2)+(x/2))
+	auto squareFunc = bind(std::multiplies<double>{},divideFunc,divideFunc);
+	// ((x/2)+(x/2)) - y
+	auto subtractFunc = bind(std::minus<double>{},squareFunc, _2);
+	// sqrt( (x/2)*(x/2) - y )
+	auto squareRootFunc = bind(sqrt,subtractFunc);
+	// -(x/2)+sqrt( (x/2)*(x/2) - y )
+	auto subtractCombinedFunc = bind(std::minus<double>{},squareRootFunc,divideFunc);
+
+	const double x{4},y{1};
+	double expected{-(x/2)+sqrt( (x/2)*(x/2) - y )};
+	double result = subtractCombinedFunc(x,y);
+	ASSERT_EQUAL(expected, result);
 }
 
 void runAllTests(int argc, char const *argv[]){
-	cute::suite s;
+	cute::suite s{};
 	//TODO add your test here
-	s.push_back(CUTE(thisIsATest));
+	s.push_back(CUTE(testSumFunctor));
+	s.push_back(CUTE(testTerm));
+	s.push_back(CUTE(testModulo));
+	s.push_back(CUTE(testSquare));
+	s.push_back(CUTE(testQuadraticEquation));
 	cute::xml_file_opener xmlfile(argc,argv);
 	cute::xml_listener<cute::ide_listener<> >  lis(xmlfile.out);
 	cute::makeRunner(lis,argc,argv)(s, "AllTests");
